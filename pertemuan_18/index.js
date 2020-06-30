fadbd3a14c76e36f77967554e4755edf62298b75
project-ant <s21810297@student.unklab.ac.id>
2020-06-30 14:45:10 +0800

    Add Pertemuan 18

---
+++ pertemuan_18/index.js
@@ -0,0 +1,19 @@
+// Store Mark's and John's Mass and Height in Variables
+// BMI = mass / height**2
+// boolean Marh > John
+// Give output in a string
+
+let Mark_height = 186;
+let Mark_mass = 70;
+
+let John_height = 180;
+let John_mass = 65;
+
+const BMI_Mark = Mark_mass / (Mark_height**2);
+const BMI_John = John_mass / (John_height**2);
+
+let hasil = console.log(BMI_Mark > BMI_John);
+
+let print = hasil === BMI_Mark ? "Mark memiliki BMI lebih besar daripada John" : "John memiliki BMI lebih besar daripada Mark";
+
+console.log(print)
