705ccf26da3d4aeb373fb43c2669f97969da5ce0
project-ant <s21810297@student.unklab.ac.id>
2020-07-05 20:12:14 +0800

    Yeahh forgot those to saves

--- pertemuan_19/index.js
+++ pertemuan_19/index.js
@@ -45,3 +45,83 @@ switch (hari) {
   default:
     seragamSekolah = "baju bebas";
 }
+
+
+//fuction declaration
+
+function namaFungsi(){
+    // Kode yang akan dijalankan
+  }
+
+// Fungsi yang disimpan di dalam variabel
+let namaVariabelFungsi = function(){
+    // Kode yang akan dijalankan
+  }
+  
+// Fungsi yang berdiri sendiri
+function sapa() {
+    return "Selamat Pagi!";
+  }
+  
+  console.log(sapa()); // Output: Selamat Pagi!
+  
+  // Fungsi disimpan di dalam variabel
+  
+  let berkenalan = function() {
+    return "Hallo, nama saya Sarah.";
+  };
+  
+  console.log(berkenalan()); // Output: Hallo, nama saya Sarah.
+
+  
+//Misalnya seperti contoh di bawah ini:
+let umur = 21;
+
+function ulangTahun() {
+    umur += 1;
+}
+
+console.log(ulangTahun()); // Output: undefined
+
+
+
+
+const olahraga = 'basketball';
+
+function namaAtlet() {
+  let atlet = 'Lionel Messi';
+  const noMessi = 10;
+  console.log(olahraga); // Output: basketball
+  if (olahraga === 'basketball') {
+     atlet = 'Kobe Bryant';
+     const noKobe = 24;
+     console.log(olahraga); // Output: basketball
+  }
+  return atlet;
+}
+
+console.log(namaAtlet()); // Output: Kobe Bryant
+console.log(olahraga); // Output: basketball
+
+
+
+
+
+const olahraga = 'basketball';
+
+function namaAtlet() {
+  let atlet = 'Lionel Messi'; // lingkup fungsi
+  const noMessi = 10; // lingkup fungsi
+  if (olahraga === 'basketball') { 
+     atlet = 'Kobe Bryant';
+     const noKobe = 24; // lingkup blok
+     console.log(noMessi); // Output: 10  
+  }
+  console.log(noKobe); // Output: Uncaught ReferenceError: noKobe is not defined
+  return atlet;
+}
+
+namaAtlet();
+
+
+
